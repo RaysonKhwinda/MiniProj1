6229f72eb4fd7488e4e70461ab18a7e5aecf0e09
RaysonKhwinda <148476954+RaysonKhwinda@users.noreply.github.com>
2023-11-10 18:38:38 +0200

    Add files via upload

---
+++ Miniprojeto_SPA/src/views/HomeView.vue
@@ -0,0 +1,197 @@
+<script setup>
+/////import TheWelcome from '../components/TheWelcome.vue'
+// import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <body>
+
+    <header id="top">
+      <nav>
+        <ul>
+          <li><a href="#QuemEuSOu" class="link">Quem Sou Eu?</a></li>
+          <li><a href="#OQueFaco" class="link">O que faço?</a></li>
+          <li><a href="#MeusHobbies" class="link">Meus Hobbies</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <div id="QuemEuSOu" class="container-fluid col-md-10">
+        <h4>Quem sou Eu?</h4>
+        <hr>
+        <p class="p">
+          Chamo-me <b>Rayson Inácio Alexandre Mpando Khwinda</b>, licenciado em Informática com especialização em
+          Engenharia de Redes e com uma sólida experiência como docente na
+          área de tecnologia da informação. Minhas competências incluem programação, gestão de redes e sistemas de
+          informação. Destaco-me por meu compromisso inabalável com a excelência no ensino e o sucesso dos alunos. Além
+          disso, trago uma bagagem valiosa em liderança de equipes e na criação de soluções tecnológicas para empresas.
+          Meu principal objectivo é impulsionar o avanço da tecnologia e da educação, deixando um impacto positivo na
+          trajectória dos estudantes e no crescimento da instituição. Ao longo de minha carreira, tive a oportunidade de
+          leccionar em diversas instituições de ensino, tanto em níveis
+          académicos mais básicos quanto em cursos avançados de tecnologia.
+        </p>
+
+        <p class="p">
+          Minhas aulas são marcadas pelo uso de
+          abordagens práticas, tornando o aprendizado mais envolvente e relevante para os alunos. Além disso, busco
+          constantemente actualizar-me em relação às mais recentes tendências e avanços tecnológicos, a fim de garantir
+          que meus alunos estejam preparados para enfrentar os desafios do mundo da tecnologia.
+          Minha experiência em liderança inclui a coordenação de equipes de desenvolvimento de software, onde fui
+          responsável por gerenciar projectos de grande envergadura, garantindo o cumprimento de prazos e a entrega de
+          soluções de alta qualidade. Essa vivência em liderança e gestão de projectos contribui para minha capacidade de
+          desenvolver estratégias eficazes e manter equipes motivadas e produtivas.
+        </p>
+      </div>
+
+      <div id="OQueFaco" class="container-fluid col-md-10">
+        <h4>O que faço?</h4>
+        <hr>
+        <p class="p">
+          Actualmente, sou consultor de marketing digital em uma agência de renome. Minha carreira me permite
+          combinar minha criatividade com habilidades analíticas para ajudar as empresas a atingir seu público-alvo e
+          aumentar sua presença online. Isso envolve uma série de tarefas e actividades, incluindo:
+        </p>
+
+        <ol class="hobbies_faco">
+          <li>
+            <b>Análise de Dados:</b> Sou especialista em análise de dados, usando ferramentas de análise de marketing para
+            avaliar o desempenho de campanhas, sites e mídias sociais. Com base nessa análise, ajusto as estratégias para
+            maximizar o ROI (Retorno sobre o Investimento) dos clientes.
+          </li>
+
+          <li>
+            <b>SEO (Optimização de Mecanismos de Busca):</b> Ajudo as empresas a melhorar sua visibilidade online por meio
+            da
+            optimização de mecanismos de busca. Isso envolve pesquisa de palavras-chave, optimização de conteúdo e
+            construção de links.
+          </li>
+
+          <li>
+            <b>Campanhas de Publicidade:</b> Gerencio campanhas de publicidade online, como anúncios no Google Ads e no
+            Facebook Ads, para aumentar o tráfego e a conversão de visitantes em clientes.
+          </li>
+
+          <li>
+            <b>Estratégia de Mídias Sociais:</b> Desenvolvo estratégias de mídias sociais para aumentar o envolvimento e a
+            presença da marca nas redes sociais. Isso inclui a criação de calendários de postagem e o monitoramento de
+            métricas de desempenho.
+          </li>
+
+          <li>
+            <b>Aconselhamento de Marketing:</b> Ofereço orientações e aconselhamento aos meus clientes sobre as melhores
+            práticas de marketing digital e as tendências do sector. Também auxílio na definição de metas e na criação de
+            planos de marketing personalizados.
+          </li>
+
+          <li>
+            <b>Educação Contínua:</b> O mundo do marketing digital está em constante evolução. Portanto, dedico tempo à
+            minha
+            própria educação contínua, participando de cursos e conferências para me manter actualizada sobre as últimas
+            estratégias e ferramentas.
+          </li>
+        </ol>
+      </div>
+
+
+      <div id="MeusHobbies" class="container-fluid col-md-10">
+        <h4>Meus hobbies</h4>
+        <hr>
+        <p class="p">
+          Além da minha paixão pela tecnologia e educação, também sou entusiasta de alguns hobbies que enriquecem minha
+          vida pessoal. Nos meus momentos de folga, gosto de:
+        </p>
+        <ol class="hobbies_faco">
+          <li>
+            <b>Leitura:</b> Sou um ávido leitor e acredito que a leitura constante enriquece meu conhecimento e minha
+            criatividade. A leitura também me ajuda a acompanhar as tendências em tecnologia e educação.
+          </li>
+
+          <li>
+            <b>Actividades ao ar livre:</b> Para equilibrar minha vida ligada à tecnologia, pratico actividades ao ar
+            livre,
+            como caminhadas, ciclismo e corrida. Isso me ajuda a recarregar as energias e manter um estilo de vida
+            saudável.
+          </li>
+
+          <li>
+            <b>Música:</b> Sou um apreciador de música e toco um instrumento musical. A música desempenha um papel
+            importante
+            na minha vida, proporcionando inspiração e relaxamento.
+          </li>
+
+          <li>
+            <b>Programação de projectos pessoais:</b> Programar não é apenas meu trabalho, mas também um hobby. Gosto de
+            trabalhar em projectos de programação pessoal, que variam desde aplicativos pequenos até projectos de código
+            aberto.
+          </li>
+
+          <li>
+            <b>Viagens e Cultura:</b> Tenho grande interesse em explorar novos lugares e culturas. Viajar me permite
+            expandir
+            meus horizontes, aprender sobre diferentes modos de vida e absorver experiências enriquecedoras. Também me
+            inspira a incorporar elementos culturais em minha abordagem à tecnologia e ao ensino, tornando minhas aulas
+            mais diversificadas e inclusivas.
+          </li>
+
+          <li>
+            <b>Fotografia:</b> A fotografia é uma paixão que combina com minha curiosidade e gosto por viagens. Capturar
+            momentos especiais e paisagens deslumbrantes me ajuda a expressar minha criatividade de uma maneira diferente.
+          </li>
+
+          <li>
+            <b>Desenvolvimento Pessoal:</b> Estou constantemente buscando maneiras de me desenvolver pessoal e
+            profissionalmente. Isso inclui a participação em workshops, cursos e eventos relacionados à tecnologia e à
+            educação, além da leitura de livros de desenvolvimento pessoal.
+          </li>
+        </ol>
+      </div>
+
+    </main>
+
+    <footer>
+      <a href="https://web.facebook.com/?_rdc=1&_rdr" target="_blank">
+        <img src="imagens/fb.png" title="Facebook">
+      </a>
+
+      <a href="https://web.whatsapp.com/" target="_blank">
+        <img src="imagens/Whatsapp.png" title="Whatsapp">
+      </a>
+
+      <a href="https://www.instagram.com/accounts/login/" target="_blank">
+        <img src="imagens/Instagram.png" title="Instagram">
+      </a>
+
+      <a href="https://accounts.google.com/InteractiveLogin/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&flowEntry=ServiceLogin&flowName=GlifWebSignIn&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&ifkv=AVQVeyzPQHHhJeib_39edhFNDIzzEKwwsPtSeMPnps9ydkZRTaFYiiE9il-Ge_iLISvw3QyD2-QY&osid=1&passive=1209600&service=mail&theme=glif"
+        target="_blank"><img src="imagens/google.png" title="Google">
+      </a>
+
+      <a href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoicHQifQ%3D%3D%22%7D"
+        target="_blank">
+        <img src="imagens/twitter.png" title="Twitter">
+      </a>
+
+      <a href="https://mz.linkedin.com/" target="_blank">
+        <img src="imagens/Linkedin.png" title="Linkedin">
+      </a>
+
+      <a href="https://accounts.snapchat.com/accounts/v2/login" target="_blank">
+        <img src="imagens/Snapchat.png" title="Snapchat">
+      </a>
+
+      <a href="https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=16&ct=1699591506&rver=7.1.6819.0&wp=MBI_SSL&wreply=https%3A%2F%2Flw.skype.com%2Flogin%2Foauth%2Fproxy%3Fclient_id%3D360605%26redirect_uri%3Dhttps%253A%252F%252Fsecure.skype.com%252Fportal%252Flogin%253Freturn_url%253Dhttps%25253A%25252F%25252Fsecure.skype.com%25252Fportal%25252Foverview%26response_type%3Dpostgrant%26state%3D2f1ec4886b0a230838794510&lc=1033&id=293290&mkt=pt-PT&psi=skype&lw=1&cobrandid=2befc4b5-19e3-46e8-8347-77317a16a5a5&client_flight=ReservedFlight33%2CReservedFlight67"
+        target="_blank">
+        <img src="imagens/Skype.png" title="Skype">
+      </a>
+
+      <a href="https://web.telegram.org/k/" target="_blank">
+        <img src="imagens/Telegram.webp" title="Telegram">
+      </a>
+
+      <a href="#top" class="cima">
+        <img src="imagens/cima.gif">
+      </a>
+
+    </footer>
+  </body>
+</template>
